b9eb1a2c4a0412a7f96fc13d93b541cf2820b271
AssafKlein-tech <74839241+AssafKlein-tech@users.noreply.github.com>
2020-12-23 09:06:35 +0200

    סיום youngest

--- ex2/eventManager.py
+++ ex2/eventManager.py
@@ -1,19 +1,24 @@
 SRC_FILE = r".\ex2\member_list.txt"
 DST_FILE = r".\ex2\member_list_out.txt"
-K = 4
+K = 5
+AGE = 2
+NAME = 1
 
 
 def printYoungestStudents(in_file_path: str, out_file_path: str, k: int):
-    #fileCorrect(in_file_path, out_file_path)
     if k < 1:
         return -1
+    #fileCorrect(in_file_path, out_file_path)
     with open(in_file_path,"r") as rfile:
         members = rfile.readlines()
+        if (len(members) < k):
+            k = len(members)
         members.sort()
-        members.sort(key=lambda member: int((member.split(","))[2].strip(" ")))
+        members.sort(key=lambda member: int((member.split(","))[AGE].strip(" ")))
         with open(out_file_path, "w") as wfile:
-            wfile.writelines(members[:k])
-    return 1
+            for member in members[:k]:
+                wfile.write((member.split(","))[NAME] + "\n")
+    return k
 
 if __name__ == "__main__":
-    printYoungestStudents(SRC_FILE, DST_FILE, K)
+    print(printYoungestStudents(SRC_FILE, DST_FILE, K))
