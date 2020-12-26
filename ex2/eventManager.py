6b488b7dd58886c8cb08f509f72e0f2537401bb6
AssafKlein-tech <74839241+AssafKlein-tech@users.noreply.github.com>
2020-12-26 19:07:57 +0200

    AGEAVG

--- ex2/eventManager.py
+++ ex2/eventManager.py
@@ -3,10 +3,11 @@ DST_FILE = r".\ex2\member_list_out.txt"
 K = 20
 AGE = 2
 NAME = 1
-
+SEMESTER = 4
+S = 5
 
 def printYoungestStudents(in_file_path: str, out_file_path: str, k: int):
-    if k < 1:
+    if type(k) is not int or k < 1:
         return -1
     #fileCorrect(in_file_path, out_file_path)
     with open(in_file_path,"r") as rfile:
@@ -20,5 +21,24 @@ def printYoungestStudents(in_file_path: str, out_file_path: str, k: int):
                 wfile.write((member.split(","))[NAME] + "\n")
     return k
 
+
+def correctAgeAvg(in_file_path: str, semester: int):
+    if type(semester) is not int or semester < 1 :
+        return -1
+    #fileCorrect(in_file_path, out_file_path)
+    with open(in_file_path,"r") as rfile:
+        counter, total_age = 0, 0
+        for member in rfile:
+            member_seperated = member.split(",")
+            print
+            if int(member_seperated[SEMESTER].strip()) == semester:
+                counter += 1
+                total_age += int(member_seperated[AGE])
+        if counter is 0:
+            return 0
+        return float(total_age) / counter
+
+
 if __name__ == "__main__":
     print(printYoungestStudents(SRC_FILE, DST_FILE, K))
+    print(correctAgeAvg(SRC_FILE,S))
