538d21893ef17e687e5219a9ccbeae40b3969dc8
Guy <guyhacmon@gmail.com>
2020-12-26 02:02:41 +0200

    המילון מלא בערכים מהקלט בצורה נכונה וללא רווחים

--- ex2/eventManager.py
+++ ex2/eventManager.py
@@ -13,5 +13,31 @@ def printYoungestStudents(in_file_path: str, out_file_path: str, k: int):
     file_out.close()
     file_in.close()
 
+def fileCorrect(in_file_path: str, out_file_path: str):
+    info_dict={
+        'id':[],
+        'name':[],
+        'age':[],
+        'year of birth':[],
+        'semester':[]
+    }
+    file_in = open(in_file_path,"r")
+    file_out = open(out_file_path,"w")
+    stream = file_in.read()
+    one_line_stream=stream.replace('\n',',')
+    one_line_stream_no_space=" ".join(one_line_stream.split())
+    student_data_list=one_line_stream_no_space.split(',')
+    student_data_list_no_space= [student_data.strip() for student_data in student_data_list]
+    info_dict['id']=student_data_list_no_space[0::5]
+    info_dict['name']=student_data_list_no_space[1::5]
+    info_dict['age']=student_data_list_no_space[2::5]
+    info_dict['year of birth']=student_data_list_no_space[3::5]
+    info_dict['semester']=student_data_list_no_space[4::5]
+
+
+    for id in info_dict['id']
+ 
 if __name__ == "__main__":
-    printYoungestStudents(SRC_FILE, DST_FILE, K)
+    fileCorrect(SRC_FILE, DST_FILE)
+
+
