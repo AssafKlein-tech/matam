949bdb0349228dc6a4034b6ede13bdc1530e1240
Guy <guyhacmon@gmail.com>
2020-12-26 03:26:58 +0200

    בדיקת תקינות הקלט כמעט הושלמה

--- ex2/eventManager.py
+++ ex2/eventManager.py
@@ -14,7 +14,7 @@ def printYoungestStudents(in_file_path: str, out_file_path: str, k: int):
     file_in.close()
 
 def fileCorrect(in_file_path: str, out_file_path: str):
-    info_dict={
+    students_dict={
         'id':[],
         'name':[],
         'age':[],
@@ -28,14 +28,18 @@ def fileCorrect(in_file_path: str, out_file_path: str):
     one_line_stream_no_space=" ".join(one_line_stream.split())
     student_data_list=one_line_stream_no_space.split(',')
     student_data_list_no_space= [student_data.strip() for student_data in student_data_list]
-    info_dict['id']=student_data_list_no_space[0::5]
-    info_dict['name']=student_data_list_no_space[1::5]
-    info_dict['age']=student_data_list_no_space[2::5]
-    info_dict['year of birth']=student_data_list_no_space[3::5]
-    info_dict['semester']=student_data_list_no_space[4::5]
-
-
-    for id in info_dict['id']
+    students_dict['id']=student_data_list_no_space[0::5]
+    students_dict['name']=student_data_list_no_space[1::5]
+    students_dict['age']=student_data_list_no_space[2::5]
+    students_dict['year of birth']=student_data_list_no_space[3::5]
+    students_dict['semester']=student_data_list_no_space[4::5]
+    for i in range(len(students_dict['id'])):
+         if int(students_dict['id'][i])/100000000==0 or not students_dict['name'][i].isalpha() or 16<int(students_dict['age'][i])<120 or 2020-int(students_dict['age'][i])==int(students_dict['year of birth'][i]) or int(students_dict['semester'][i])>0 :
+            students_dict['id'].pop(i)
+            students_dict['name'].pop(i)
+            students_dict['age'].pop(i)
+            students_dict['year of birth'].pop(i)
+            students_dict['semester'].pop(i)
  
 if __name__ == "__main__":
     fileCorrect(SRC_FILE, DST_FILE)
