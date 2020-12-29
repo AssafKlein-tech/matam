36bf401a5a2c168955b086c77f4974dfd2562c8c
Guy <guyhacmon@gmail.com>
2020-12-29 18:23:35 +0200

    stringcorrect נוצרה ועובדת

--- ex2/eventManager.py
+++ ex2/eventManager.py
@@ -6,7 +6,7 @@ NAME = 1
 SEMESTER = 4
 S = 6
 
-def fileCorrect(in_file_path: str, out_file_path: str):
+def stringCorrect(stream: str):
     students_dict={
         'id':[],
         'name':[],
@@ -14,9 +14,6 @@ def fileCorrect(in_file_path: str, out_file_path: str):
         'year of birth':[],
         'semester':[]
     }
-    file_in = open(in_file_path,"r")
-    file_out = open(out_file_path,"w")
-    stream = file_in.read()
     one_line_stream=stream.replace('\n',',')
     one_line_stream_no_space=" ".join(one_line_stream.split())
     student_data_list=one_line_stream_no_space.split(',')
@@ -26,6 +23,8 @@ def fileCorrect(in_file_path: str, out_file_path: str):
     students_dict['age']=student_data_list_no_space[2::5]
     students_dict['year of birth']=student_data_list_no_space[3::5]
     students_dict['semester']=student_data_list_no_space[4::5]
+   
+   
    
     for i in range(len(students_dict['id'])):
         if int(students_dict['id'][i])//10000000==0 or not students_dict['name'][i].replace(' ','').isalpha() or not 16<int(students_dict['age'][i])<120 or not 2020-int(students_dict['age'][i])==int(students_dict['year of birth'][i]) or not int(students_dict['semester'][i])>0 :
@@ -45,15 +44,27 @@ def fileCorrect(in_file_path: str, out_file_path: str):
                     students_dict['year of birth'][j]=None
                     students_dict['semester'][j]=None
 
+    
     sorted_id_list=students_dict['id'].copy()
     sorted_id_list = [int(i) for i in sorted_id_list if i!=None ]
     sorted_id_list.sort()     
 
+    s=""
     for id in sorted_id_list:
          for i in range(len(students_dict['id'])-1,0,-1):
              if students_dict['id'][i]!=None and id==int(students_dict['id'][i]):
-                file_out.write(students_dict['id'][i]+', '+students_dict['name'][i]+', '+students_dict['age'][i]+', '+students_dict['year of birth'][i]+', '+students_dict['semester'][i]+'\n')
+                s+=students_dict['id'][i]+', '+students_dict['name'][i]+', '+students_dict['age'][i]+', '+students_dict['year of birth'][i]+', '+students_dict['semester'][i]+'\n'
+    return s
 
+
+
+def fileCorrect(in_file_path: str, out_file_path: str):
+    
+    file_in = open(in_file_path,"r")
+    file_out = open(out_file_path,"w")
+    stream = file_in.read()
+    s = stringCorrect(stream)
+    file_out.write(s)
     file_out.close()
     file_in.close()
 
