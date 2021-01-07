916ff50723fcff7d5c7f244404fbebc871b24bd9
Guy Hakmon <guyhakmon@campus.technion.ac.il>
2021-01-07 21:02:09 +0200

    יש עוד קצת עבודה על הקובץ H

--- ex3/date_wrap.h
+++ ex3/date_wrap.h
@@ -1,3 +1,30 @@
-namespace mtm{
-    class 
-}
+#ifndef DATE_WRAP_H_
+#define DATE_WRAP_H_
+
+#include <stdbool.h>
+
+class DateWrap{
+    Date date;
+};
+public:
+
+DateWrap::DateWrap(int day, int month, int year);
+DateWrap::DateWrap(int date);
+
+int day();
+int month();
+int year();
+printDateWarp();
+bool operator==(const DateWrap& datewrap1, const DateWrap& datewrap2);
+bool operator>(const DateWrap& datewrap1, const DateWrap& datewrap2);
+bool operator<(>const DateWrap& datewrap1, const DateWrap& datewrap2);
+bool operator<=(const DateWrap& datewrap1, const DateWrap& datewrap2);
+bool operator>=(const DateWrap& datewrap1, const DateWrap& datewrap2);
+bool operator!=(const DateWrap& datewrap1, const DateWrap& datewrap2);
+bool operator++(const DateWrap& datewrap);
+DateWrap& operator++(const DateWrap& datewrap);
+DateWrap& operator+=(const DateWrap& datewrap, int days_to_add);
+DateWrap operator+(const DateWrap& datewrap);
+
+
+
