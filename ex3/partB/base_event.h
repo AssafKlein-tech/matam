da1d858ff1cd445771eca1422c992a5345cc58e7
Guy <guyhacmon@gmail.com>
2021-01-10 21:28:25 +0200

    s

--- ex3/partB/base_event.h
+++ ex3/partB/base_event.h
@@ -1,15 +1,28 @@
 #ifndef BASE_EVENT_H_
 #define BASE_EVENT_H_
-#include <iostream>
+#include<string.h>
 using std::ostream;
-#include <stdbool.h>
+#include <iostream>
+#include "date_wrap.h"
 
 namespace mtm{
     class BaseEvent{
-         
-
+         DateWrap date;
+         std::string name;
+         struct Members_list
+    {
+        int member;
+        struct Members_list *next;
+    } *members; 
+     
     public:
-
+    BaseEvent(DateWrap date, std::string name);
+    registerParticipant(int student)
+    unregisterParticipant(int student)
+    printShort(std::ostream stream)
+    printLong(std::ostream stream)
+    clone()
+    
     protected:    
 
     };
