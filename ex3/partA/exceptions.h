bee084fa97aba7f3e45353fc8dc2e7d8e826fda7
assaf server <assafklein@campus.technion.ac.il>
2021-01-16 12:31:50 +0200

    exceptions

--- ex3/partA/exceptions.h
+++ ex3/partA/exceptions.h
@@ -6,15 +6,26 @@ using std::exception;
 
 namespace mtm{
     class Exception: public exception {};
+    
     //DateWrap exceptions
     class InvalidDate: public Exception {};
     class NegativeDays: public Exception {};
 
-    //EventContainer exceptions
+    //Events exceptions
+    class AlreadyRegistered: public Exception {};
+    class RegistrationBlocked: public Exception {};
+    class NotRegistered: public Exception {};
+    class AlreadyInvited: public Exception {};
+
+    //EventContainers exceptions
     class NotSupported: public Exception {};
     class DateMismatch: public Exception {};
     class InvalidNumber: public Exception {};
     class InvalidInterval: public Exception {};
+
+    //schedule exceptions
+    class EventAlreadyExists: public Exception {};
+    class EventDoesNotExist: public Exception {};
 }
 
 #endif
